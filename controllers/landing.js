27e6044118b49a45f98f36624ed7e0602af183d7
Brian Davidson <briandaviddavidson@gmail.com>
2020-04-23 14:52:05 -0400

    Add front end lead deletion with ajax

--- controllers/landing.js
+++ controllers/landing.js
@@ -59,3 +59,13 @@ exports.deleteLead = function(req, res, next) {
     res.redirect('/leads');
   })
 };
+
+exports.deleteLeadJson = function(req, res, next) {
+  return models.Lead.destroy({
+    where: {
+      id: req.params.leadId
+    }
+  }).then(result => {
+    res.send({msg: "Success"});
+  })
+};
--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/lead/:leadId', landing.showLead);
 router.get('/lead/:leadId/edit', landing.showEditLead);
 router.post('/lead/:leadId/edit', landing.EditLead);
 router.post('/lead/:leadId/delete', landing.deleteLead);
+router.post('/lead/:leadId/delete-json', landing.deleteLeadJson);
 
 module.exports = router;
