e3645875c0fbef45afb1758d145d2fa2280915e3
Brian Davidson <briandaviddavidson@gmail.com>
2020-04-23 15:59:03 -0400

    Add user routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 let landing = require('../controllers/landing');
+let user = require('../controllers/user');
 
-/* GET home page. */
+// Login routes
+router.get('/login', user.showLogin);
+router.get('/signup', user.showSignup);
+// Sales pages
 router.get('/', landing.getLanding);
 router.post('/', landing.submitLead);
 router.get('/leads', landing.showLeads);
